2c66b2b9142d6eec830daa0abc0a0ccf96d3eb50
CaoKai <2655257336@qq.com>
2025-04-06 13:54:51 +0800

    添加变量和函数，更新控制台输出

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log("666");
+let a = "Hello World";
+const count = (x, y) => {
+    return x + y;
+};
+console.log(count(1, 2));
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,1 +1,7 @@
-console.log("666");
+let a: string = "Hello World";
+
+const count = (x: number, y: number): number => {
+  return x + y;
+};
+
+console.log(count(1, 2));
