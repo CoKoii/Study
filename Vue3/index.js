e0260e8970b20dc827765f5ffe97698c2a12ef54
CaoKai <2655257336@qq.com>
2025-04-12 17:17:14 +0800

    重构注册效果逻辑，优化清理和触发机制，修复变量命名并更新响应式输出

--- Vue3/index.js
+++ Vue3/index.js
@@ -3,8 +3,21 @@ const bucket = new WeakMap();
 let activeEffect;
 
 const registerEffect = (fn) => {
-  activeEffect = fn;
-  fn();
+  const effect = () => {
+    cleanup(effect);
+    activeEffect = effect;
+    fn();
+  };
+  effect.deps = [];
+  effect();
+  console.dir(effect);
+};
+const cleanup = (effect) => {
+  for (let i = 0; i < effect.deps.length; i++) {
+    const deps = effect.deps[i];
+    deps.delete(effect);
+  }
+  effect.deps.length = 0;
 };
 const track = (target, key) => {
   if (!activeEffect) return target[key];
@@ -17,21 +30,24 @@ const track = (target, key) => {
     map.set(key, (set = new Set()));
   }
   set.add(activeEffect);
+  activeEffect.deps.push(set);
+  console.log(bucket);
 };
 
 const trigger = (target, key) => {
   const map = bucket.get(target);
   if (!map) return true;
   const functions = map.get(key);
-  if (functions) {
-    functions.forEach((fn) => {
+  const functionsToRun = new Set(functions);
+  if (functionsToRun) {
+    functionsToRun.forEach((fn) => {
       fn();
     });
   }
   return true;
 };
 
-const number = { num: 0 };
+const number = { num: 0, ok: true };
 
 const count = new Proxy(number, {
   get: (target, key) => {
@@ -45,7 +61,7 @@ const count = new Proxy(number, {
 });
 
 registerEffect(() => {
-  document.querySelector(".num").textContent = count.num;
+  document.querySelector(".num").innerHTML = count.ok ? count.num : -1;
 });
 document.querySelector(".add").addEventListener("click", () => {
   count.num++;
