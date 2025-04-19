7c1749c9c440c384270116c0ea81fcfa4c78cfdf
CaoKai <2655257336@qq.com>
2025-04-19 15:47:37 +0800

    feat: 添加Person对象，包含个人信息并输出相关属性

--- JavaScript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ JavaScript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,14 @@
 "use strict";
 
-const array = [1, 2, 3];
-console.log(array.indexOf(3));
+const Person = {
+  name: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+
+console.log(Person.name);
+console.log(Person["name"]);
+const nameKey = "Name";
+console.log(Person["last" + nameKey]);
