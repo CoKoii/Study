8199640210026be91352239ed615aec2a6f0d735
CaoKai <2655257336@qq.com>
2025-04-20 22:01:29 +0800

    feat: 重构代码，添加最大值和最小值计算逻辑

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,11 @@
-1 < 2 ? console.log(1) : console.log(2);
+"use strict";
+
+const number = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+let max = number[0];
+let min = number[0];
+for (let i = 0; i < number.length; i++) {
+  if (number[i] > max) max = number[i];
+  if (number[i] < min) min = number[i];
+}
+console.log(`Max: ${max}`);
+console.log(`Min: ${min}`);
