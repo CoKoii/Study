c0de888b8bdfc9352565cb88ed9b06aa841d7600
CaoKai <2655257336@qq.com>
2025-04-21 09:59:56 +0800

    feat: 添加猜数字游戏逻辑和温度转换功能

--- JavaScript/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ JavaScript/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const guessNumber = parseInt(Math.random() * 20 + 1);
+
+const checkBtn = () => {
+  const guess = document.querySelector('.guess').value;
+  if (guess < guessNumber) {
+    document.querySelector('.message').innerHTML = 'Too low';
+    document.querySelector('.score').innerHTML--;
+  } else if (guess > guessNumber) {
+    document.querySelector('.message').innerHTML = 'Too high';
+    document.querySelector('.score').innerHTML--;
+  } else {
+    document.querySelector('.message').innerHTML = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').innerHTML = guessNumber;
+  }
+};
+document.querySelector('.check').addEventListener('click', checkBtn);
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,13 @@
 "use strict";
 
-const number = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
-let max = number[0];
-let min = number[0];
-for (let i = 0; i < number.length; i++) {
-  if (number[i] > max) max = number[i];
-  if (number[i] < min) min = number[i];
-}
-console.log(`Max: ${max}`);
-console.log(`Min: ${min}`);
+const measureKelvin = () => {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: prompt("Degrees Celsius: "),
+  };
+  console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
