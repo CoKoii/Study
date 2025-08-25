fb5a96437e60173e4cd122284628581276877339
CK <2655257336@qq.com>
2025-08-25 19:12:59 +0800

    重构 App 组件，移除动态组件切换功能，删除 Box-one 和 Box-second 组件，简化代码结构

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
-<script setup>
-import Box1 from './views/Box-one.vue'
-import Box2 from './views/Box-second.vue'
-import { ref } from 'vue'
-
-const current = ref('Box1')
-const changeComponent = () => {
-  current.value = current.value === 'Box1' ? 'Box2' : 'Box1'
-}
-
-const components = { Box1, Box2 }
+<script setup lang="ts">
+defineOptions({
+  name: 'App',
+})
 </script>
 
 <template>
-  <component :is="components[current]" :key="current"></component>
-  <button @click="changeComponent">change</button>
+  <div class="App"></div>
 </template>
+
+<style scoped lang="scss"></style>
