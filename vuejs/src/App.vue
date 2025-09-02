0685d2f484a599ea5d71d962e0096f61781bc2f5
CK <2655257336@qq.com>
2025-09-02 21:16:41 +0800

    nestjs初始化

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 defineOptions({
   name: 'App',
 })
+const count = ref(0)
+setInterval(() => {
+  count.value++
+}, 1000)
 </script>
 
 <template>
-  <div class="App"></div>
+  <div class="App" v-once>{{ count }}</div>
 </template>
 
 <style scoped lang="scss"></style>
