8089eae303740243509946bf033076a845e9396d
CK. <2655257336@qq.com>
2025-09-22 19:50:06 +0800

    新增 index.js 文件，添加用户、轮播图和视频列表的异步加载功能；新增文本文件 1.txt，包含诗句内容

--- html/index.js
+++ html/index.js
@@ -1,3 +1,43 @@
 "use strict";
-
-console.log(navigator.plugins);
+const getUserList = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        { name: "张三", age: 18 },
+        { name: "李四", age: 19 },
+        { name: "王五", age: 20 },
+      ]);
+    }, 1000);
+  });
+};
+const getBannerList = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        { name: "Banner1", id: 1 },
+        { name: "Banner2", id: 2 },
+        { name: "Banner3", id: 3 },
+      ]);
+    }, 2000);
+  });
+};
+const getVideoList = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        { title: "JavaScript", id: 1 },
+        { title: "TypeScript", id: 2 },
+        { title: "Vue", id: 3 },
+      ]);
+    }, 3000);
+  });
+};
+const initLoad = () => {
+  Promise.all([getUserList(), getBannerList(), getVideoList()]).then((res) => {
+    console.log(res);
+    console.log("用户列表", res[0]);
+    console.log("轮播图列表", res[1]);
+    console.log("视频列表", res[2]);
+  });
+};
+initLoad();
