812ed9068c4125094dc23cdbcf33936a52e8e636
CaoKai <2655257336@qq.com>
2025-09-23 09:56:07 +0800

    重构 index.js 中的 ajax 函数以支持 Promise，新增 1.txt、2.txt 和 3.txt 文件

--- html/index.js
+++ html/index.js
@@ -1,14 +1,24 @@
-"use strict";
+const ajax = (url) => {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.send(null);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+          resolve(xhr.responseText);
+        } else {
+          reject(xhr.statusText);
+        }
+      }
+    };
+  });
+};
 
-const p1 = new Promise((resolve, reject) => {
-  resolve("ok");
-});
-const result = p1.then(
-  (value) => {
-    return value;
-  },
-  (error) => {
-    return error;
-  }
-);
-console.log(result);
+Promise.allSettled([ajax("/api/users"), ajax("/api/urls")])
+  .then((results) => {
+    console.log(results);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
