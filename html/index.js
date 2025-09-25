4c02d97a56f5639448fe968a4e7eb386382c1a4e
CK. <2655257336@qq.com>
2025-09-25 13:04:13 +0800

    移除 AJAX 请求代码，保留文档输出

--- html/index.js
+++ html/index.js
@@ -1,24 +1,3 @@
-const ajax = (url) => {
-  return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.send(null);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
-          resolve(xhr.responseText);
-        } else {
-          reject(xhr.statusText);
-        }
-      }
-    };
-  });
-};
+"use strict";
 
-Promise.allSettled([ajax("/api/users"), ajax("/api/urls")])
-  .then((results) => {
-    console.log(results);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+console.dir(document);
