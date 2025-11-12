3b08cf577c72bd8b82c34f0e539b1fca853789d0
CK. <2655257336@qq.com>
2025-11-12 16:37:55 +0800

    nestjs初学

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,26 +1,12 @@
-import { Body, Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/api')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('app')
-  getApp(): string {
-    return 'Hello Nestjs';
-  }
-
-  @Get('range')
-  getRange(@Query() query: { num: number }): number[] {
-    const arr: number[] = [];
-    for (let i = 0; i < query.num; i++) {
-      arr.push(i + 1);
-    }
-    return arr;
   }
 }
