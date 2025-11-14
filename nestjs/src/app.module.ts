1280d10c45a77048c08ffbbe473dcfecea8bc32d
CK. <2655257336@qq.com>
2025-11-14 18:55:47 +0800

    feat: add PostCSS configuration and TypeScript setup for chat app
    chore: create environment configuration files for NestJS development and production
    
    refactor: define database configuration enums in NestJS

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-
+import { ConfigModule } from '@nestjs/config';
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- nestjs/src/user/user.controller.ts
+++ nestjs/src/user/user.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigEnum } from 'src/enum/config.enum';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {}
   @Get()
   getHello(): string {
+    const data = this.configService.get<string>(ConfigEnum.DB);
+    console.log('Database Host:', data);
     return this.userService.getHello();
   }
 }
