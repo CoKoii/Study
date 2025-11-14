b75c14c9bad8f64956335ca8d4c360fc65c89f81
CK. <2655257336@qq.com>
2025-11-14 21:58:19 +0800

    typeorm配置

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigEnum } from './enum/config.enum';
 const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get<'mysql'>(ConfigEnum.DB_TYPE),
+          host: configService.get<string>(ConfigEnum.DB_HOST),
+          port: configService.get<number>(ConfigEnum.DB_PORT),
+          username: configService.get<string>(ConfigEnum.DB_USERNAME),
+          password: configService.get<string>(ConfigEnum.DB_PASSWORD),
+          database: configService.get<string>(ConfigEnum.DB_NAME),
+          entities: [],
+          synchronize: true,
+          logging: ['error'],
+        }) as TypeOrmModuleOptions,
+    }),
     UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
--- nestjs/src/enum/config.enum.ts
+++ nestjs/src/enum/config.enum.ts
@@ -1,4 +1,8 @@
 export enum ConfigEnum {
-  DB = 'DB',
+  DB_TYPE = 'DB_TYPE',
   DB_HOST = 'DB_HOST',
+  DB_PORT = 'DB_PORT',
+  DB_USERNAME = 'DB_USERNAME',
+  DB_PASSWORD = 'DB_PASSWORD',
+  DB_NAME = 'DB_NAME',
 }
