5af0fbc7b70c168c2ab0062673d3fddd1d858bbd
CK. <2655257336@qq.com>
2025-11-23 10:43:17 +0800

    feat: 添加 request-ip 依赖，更新 HttpExceptionFilter 以增强异常处理和日志记录

--- nestjs/src/common/filters/http-exception.filter.ts
+++ nestjs/src/common/filters/http-exception.filter.ts
@@ -3,22 +3,39 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { HttpAdapterHost } from '@nestjs/core';
+import { Request, Response } from 'express';
 import { Logger } from 'nestjs-pino';
 
-@Catch(HttpException)
+@Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-  constructor(private logger: Logger) {}
-  catch(exception: HttpException, host: ArgumentsHost) {
+  constructor(
+    private readonly logger: Logger,
+    private readonly httpAdapterHost: HttpAdapterHost,
+  ) {}
+  catch(exception: unknown, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
-    const status = exception.getStatus();
-    this.logger.error(exception.message, exception.stack);
-    response.status(status).json({
-      code: status,
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = {
+      headers: request.headers,
+      query: request.query,
+      body: request.body as unknown,
+      params: request.params,
       timestamp: new Date().toISOString(),
-      message: exception.message || exception.name,
-    });
+      ip: request.ip || request.socket.remoteAddress,
+      exception: (exception as Error)?.name,
+      error: (exception as Error)?.message || 'Internal Server Error',
+    };
+    this.logger.error('[Exception]', responseBody);
+    httpAdapter.reply(response, responseBody, httpStatus);
   }
 }
--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { Logger } from 'nestjs-pino';
@@ -7,7 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useLogger(app.get(Logger));
-  app.useGlobalFilters(new HttpExceptionFilter(app.get(Logger)));
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(
+    new HttpExceptionFilter(app.get(Logger), httpAdapterHost),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
