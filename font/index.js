7033667ddaf95c7980dc660d447514586ae27fed
CK. <2655257336@qq.com>
2026-02-27 09:32:42 +0800

    chore: clean up code structure and remove unused code blocks

--- font/index.js
+++
@@ -1,9 +0,0 @@
-const createFontSlice = require('font-slice');
-const path = require('path');
-
-createFontSlice({
-  // fontPath
-  fontPath: path.resolve(__dirname, 'LXGWWenKai-Medium.ttf'),
-  // outputDir
-  outputDir: path.resolve(__dirname, './output'),
-})
