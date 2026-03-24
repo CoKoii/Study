8c96757663decc9fb9300ebde631aae3509bfc36
CK. <2655257336@qq.com>
2026-03-24 12:21:09 +0800

    feat: initialize demo project with Rollup and Redis setup
    - Added package.json for demo project with Rollup as a dev dependency.
    - Created pnpm-lock.yaml for dependency management.
    - Implemented utility functions in utils.js.
    - Added output.js as an initial output file.
    - Set up Docker Compose for Redis with password protection and persistent storage.

--- Z/src/index.js
+++
@@ -1,7 +0,0 @@
-import "jquery";
-import { get } from "lodash";
-import "./css/index.css";
-import "./css/public.css";
-import "./js/nav";
-import "./js/public";
-console.log(get({ a: 1 }, "a"));
---
+++ demo/input.js
@@ -0,0 +1,3 @@
+import { foo } from "./utils.js";
+const obj = {};
+/*#__PURE__*/ foo(obj);
