d237be250db23bd452d43291e142adf6a2c1e4a8
CK. <2655257336@qq.com>
2026-08-11 09:28:58 +0800

    feat: 添加天气查询功能及状态管理中间件

---
+++ Agent/langchain/agents/index.ts
@@ -0,0 +1,50 @@
+import { createAgent, tool, createMiddleware } from "langchain";
+import { ChatOpenAI } from "@langchain/openai";
+import * as z from "zod";
+import { StateSchema } from "@langchain/langgraph";
+// 自定义一个工具函数，用于获取指定城市的天气信息
+const getWeather = tool((input) => `${input.city}的天气永远是晴天！`, {
+  name: "get_weather",
+  description: "获取指定城市的天气",
+  schema: z.object({
+    city: z.string().describe("要查询天气的城市"),
+  }),
+});
+// 定义一个响应格式，用于返回天气信息和置信度
+const Answer = z.object({
+  summary: z.string(),
+  confidence: z.number(),
+});
+
+// 定义一个状态模式，用于记录用户ID和调用次数
+const MyState = new StateSchema({
+  userId: z.string().describe("用户ID"),
+  callCount: z.number().describe("调用次数"),
+});
+
+// 创建一个状态中间件，用于扩展智能体的状态
+const stateMiddleware = createMiddleware({
+  name: "StateExtension",
+  stateSchema: MyState,
+});
+// 创建一个 ChatOpenAI 模型实例，使用 deepseek-v4-flash 模型，并设置 API Key 和 Base URL
+const model = new ChatOpenAI({
+  model: "deepseek-v4-flash",
+  apiKey: process.env.OPENAI_API_KEY,
+  configuration: { baseURL: process.env.OPENAI_API_BASE_URL },
+  modelKwargs: { enable_thinking: false },
+});
+
+// 创建一个智能体实例，传入模型和工具函数，并指定响应格式
+const agent = createAgent({
+  model,
+  tools: [getWeather],
+  responseFormat: Answer,
+  middleware: [stateMiddleware],
+});
+
+const res = await agent.invoke({
+  messages: [{ role: "user", content: "你好啊" }],
+});
+
+console.log(res.structuredResponse);
